ee4229588973f0137c0246ae536101a07d9bb9fd
Cesar Caceres <cesarc770@gmail.com>
2018-01-11 12:49:32 -0700

    added passport for Auth strategy

---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0').Strategy;
+const keys = require('../config/keys');
+
+passport.use(
+	new Auth0Strategy(
+	{
+	domain: keys.auth0domain,
+	clientID: keys.auth0ClientID,
+	clientSecret: keys.auth0ClientSecret,
+	callbackURL: '/callback'
+}, function(accessToken, refreshToken, profile, done){
+	console.log('access token: ', accessToken);
+	console.log('refreh token: ', refreshToken);
+	console.log('profile: ', profile);
+
+})
+);
