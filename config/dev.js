6b9d900e8405b5c0fc418760a53e32c696f69b64
Cesar Caceres <cesarc770@gmail.com>
2018-01-17 21:16:16 -0700

    update to keys

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,9 @@
 module.exports = {
 	auth0domain: "crash-pad-dev.auth0.com",
 	auth0ClientID: "ElzlYyFwPFsXphLzvXLgj7-p_6HI0U95",
-	auth0ClientSecret: "Av9mRkzZ1aWYwWB44qxN5IoTUPI1pmMwLGnCJ4l2XYZogan8WqgyKfMI7GakhRXI"
+	auth0ClientSecret: "Av9mRkzZ1aWYwWB44qxN5IoTUPI1pmMwLGnCJ4l2XYZogan8WqgyKfMI7GakhRXI",
+	auth0domainManager: "crash-pad-dev.auth0.com",
+	auth0ClientIDManager: "pV07tjX84J3mOU4ubE8ikH4vreT2v2Mq",
+	auth0ClientSecretManager: "T4VNz5QAhyLY7pJJamguyCkly1eyxTOZpyA8-bQLF0pOe_DOX5zmJJ74_sXiVsXJ",
 	//add uri and cookie key if needed
 }
