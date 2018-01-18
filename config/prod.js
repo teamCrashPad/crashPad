c0e4bd524024921fcf5d68092fa7d595e00b1898
Cesar Caceres <cesarc770@gmail.com>
2018-01-17 21:12:36 -0700

    adding more auth routes

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,9 @@
 module.exports = {
 	auth0domain: process.env.AUTH0_DOMAIN,
 	auth0ClientID: process.env.AUTH0_CLIENT_ID,
-	auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET
+	auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET,
+	auth0domainManager: process.env.AUTH0_DOMAIN_MANAGER,
+	auth0ClientIDManager: process.env.AUTH0_CLIENT_ID_MANAGER,
+	auth0ClientSecretManager: process.env.AUTH0_CLIENT_SECRET_MANAGER,
 	//add uri and cookie key if needed
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,9 +13,27 @@ module.exports = function(app){
   }
 );
 
+  app.get(
+  '/landlord/callback',
+  passport.authenticate('auth0', {
+    failureRedirect: '/'
+  }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/');
+  }
+);
+
 	app.get('/login', passport.authenticate('auth0', {scope: ['profile', 'email']}), function(req, res){
 		res.redirect("/");
 	});
 
 
 };
+
+app.get('/lanlord/login', passport.authenticate('auth0', {scope: ['profile', 'email']}), function(req, res){
+    res.redirect("/");
+  });
+
+
+};
+
