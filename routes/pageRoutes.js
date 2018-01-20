b4b9566df2cee85a1eafd890837c79c001331364
Jacob Hershberger <whyisjacob@gmail.com>
2018-01-19 17:56:25 -0700

    udpate

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,16 +1,18 @@
 const requireLogin = require('../middlewares/requireLogin');
 const requireLandlord = require('../middlewares/requireLandlord');
-module.exports = function(app){
-    app.get("/", function(req, res){
+module.exports = function (app) {
+    app.get("/", function (req, res) {
         // res.render("index");
-         res.render('index', {
-    user: req.user,
-    userProfile: JSON.stringify(req.user, null, '  ')
-  });
+        res.render('index', {
+            layout: 'homepage',
+            user: req.user,
+            userProfile: JSON.stringify(req.user, null, '  ')
+
+        });
 
     })
 
-    app.get("/landlord", requireLogin, requireLandlord, function(req, res){
+    app.get("/landlord", requireLogin, requireLandlord, function (req, res) {
         res.render("landlord");
     })
 }
