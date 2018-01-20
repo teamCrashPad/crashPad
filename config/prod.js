1268bd2c3022be306e02e61dbfd22ca00b55a574
Cesar Caceres <cesarc770@gmail.com>
2018-01-20 09:58:58 -0700

    config update

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,8 @@ module.exports = {
 	auth0ClientID: process.env.AUTH0_CLIENT_ID,
 	auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET,
 	cookieKey: 'ihsfjhsdfsdjhfsjdhfjshdfjkshdfksskdhfjksd',
-	auth0Audience: ''
+	auth0Audience: 'https://crash-pad-dev.auth0.com/api/v2/',
+	auth0TokenURL: 'https://crash-pad-dev.auth0.com/oauth/token',
+	auth0EmailURL: 'https://crash-pad-dev.auth0.com/api/v2/users-by-email'
 	//add uri and cookie key if needed
 }
