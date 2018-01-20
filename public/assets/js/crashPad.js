2a5f6767b290ed2a48a72916cfa3735bea014813
Curtis Lytle <curtlytle@gmail.com>
2018-01-20 12:10:28 -0700

    Cleared out unnecessary code.  This can be used for generic purposes, so we'll keep it for now.

--- public/assets/js/crashPad.js
+++ public/assets/js/crashPad.js
@@ -1,24 +1,3 @@
-$(function() {
-    $(".application-form").on("submit", function(event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-
-        var tenantInfo = {
-            firstName: $("#fname").val().trim(),
-            lastName: $("#lname").val().trim(),
-            phone: $("#phn").val().trim()
-        };
-
-        // Send the POST request.
-        $.ajax("/api/??", {
-            type: "POST",
-            data: tenantInfo
-        }).then(
-            function() {
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
-});
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+});
