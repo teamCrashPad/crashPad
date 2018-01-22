6552365af8b3c1f79775e3d95c7bba5dd5c0104b
Curtis Lytle <curtlytle@gmail.com>
2018-01-22 13:05:34 -0700

    Added description to Property

--- models/Property.js
+++ models/Property.js
@@ -7,6 +7,10 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1]
             }
         },
+        description: {
+            type:  DataTypes.TEXT,
+            allowNull: true
+        },
         price: {
             type: DataTypes.STRING
         },
