7326f0797c7e70debf3a1a58148e0f51b2856d00
Andrew Reyes <Andyrc6@gmail.com>
2018-01-22 12:26:13 -0700

    adding to landlords dashboard

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,5 +1,7 @@
 const requireLogin = require('../middlewares/requireLogin');
 const requireLandlord = require('../middlewares/requireLandlord');
+var db = require("../models");
+
 module.exports = function (app) {
     app.get("/", function (req, res) {
         // res.render("index");
@@ -13,7 +15,29 @@ module.exports = function (app) {
     })
 
     app.get("/landlord", requireLogin, requireLandlord, function (req, res) {
-        res.render("landlord", {name: req.user.firstName});
+        var properties;
+        var isProperties
+        
+        db.Property.findAll({
+            include: [db.Landlord],
+            where: {
+                LandlordId: req.user.id
+            }
+
+        }).then(function(data){
+            isProperties = data.length;
+            properties = data;
+            console.log(properties);
+            console.log(isProperties);
+
+            res.render("landlord", {name: req.user.firstName,
+                properties: properties,
+                isProperties: isProperties
+                });
+        });
+
+
+        
     })
 
     app.get("/search/:search_query", function (req, res) {
