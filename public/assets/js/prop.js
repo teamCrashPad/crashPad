8472f3ac11dfd343d9b141c06089548516391f0c
Curtis Lytle <curtlytle@gmail.com>
2018-01-22 21:34:10 -0700

    Fixing syntax

--- public/assets/js/prop.js
+++ public/assets/js/prop.js
@@ -2,17 +2,16 @@ $(document).ready(function () {
     $("#applyFor").on("click", function () {
         var propId = $(this).attr("data-id");
         console.log(".... Clicked on apply for Prop #: " + propId);
-        $.get("/api/current_user", function(data){
+        $.get("/api/current_user", function (data) {
             myid = data.id;
             var propObj = {
                 "PropertyId": propId,
                 "TenantId": myid
-                }
-            };
+            }
+        });
 
-            $.get("/api/properties", propObj, function(data, status){
-                console.log(status);
-            });
+        $.get("/api/properties", propObj, function (data, status) {
+            console.log(status);
         });
     });
 });
