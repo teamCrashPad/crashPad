ae7fb64397e50ba689e16c6437c913b16af38e6a
Curtis Lytle <curtlytle@gmail.com>
2018-01-23 02:05:24 -0700

    adding node local storage

--- routes/application-api-routes.js
+++ routes/application-api-routes.js
@@ -1,10 +1,16 @@
 var db = require("../models");
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
 
 module.exports = function (app) {
     app.get("/api/application", function (req, res) {
     });
 
     app.get("/api/fillApplication/:propId", function (req, res) {
+        var propId = req.params.propId;
+        localStorage.setItem("CPADpropId", propId);
         //var myId = req.user.id;
         console.log("filling app....");
         var myId = 1;
@@ -17,16 +23,22 @@ module.exports = function (app) {
             console.log("...dbTemplate: ");
             console.log(dbTemplate);
             res.render("applicationDetail", {
-                comments: dbTemplate.comments,
+                mycomments: dbTemplate.comments,
                 pets:  dbTemplate.havePets,
                 smokes:  dbTemplate.isSmoker
             });
         });
     });
 
-    app.post("/api/application", function (req, res) {
+    app.post("/api/submitApplication", function (req, res) {
+        var myId = req.user.id;
+        var propId = localStorage.getItem("CPADpropId");
+        var myApplication = {
+            tenantId: myId,
+            propertyId: propId
+        };
         //console.log(req.body);
-        db.Application.create(req.body, {include: [db.ApplicationTemplate, db.Property]}).then(function (dbApplication) {
+        db.Application.create(myApplication, {include: [db.Tenant, db.Property]}).then(function (dbApplication) {
             res.json(dbApplication);
         });
     });
